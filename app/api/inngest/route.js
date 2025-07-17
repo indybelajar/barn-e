530fb2afa5dd2db927f69cbd04a9246561d2a125
indybelajar <indyagustin06@gmail.com>
2025-07-17 12:01:25 +0700

    delete inngest functions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,8 +1,7 @@
 import { serve } from "inngest/next";
 import { inngest, syncuserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
-import { helloWorld } from "../../../inngest/functions";
-
-export const { GET, POST, PUT } = serve({
+// test 
+export const { GET, POST, PUT, } = serve({
   client: inngest,
   functions: [
     syncuserCreation,
