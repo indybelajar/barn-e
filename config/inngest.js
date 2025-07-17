cd281dbd110be0d64ff6b7f75d5af89f37bf0628
indybelajar <indyagustin06@gmail.com>
2025-07-17 13:10:17 +0700

    typo emaill

--- config/inngest.js
+++ config/inngest.js
@@ -35,7 +35,7 @@ export const syncUserUpdation = inngest.createFunction(
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
     const userData = {
             _id:id,
-            email: email-addresses[0].email_address,
+            email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             imageUrl: image_url
         }
