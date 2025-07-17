269504fac26e2f5dee721fe7f837148dba6093e5
indybelajar <indyagustin06@gmail.com>
2025-07-17 14:21:03 +0700

    address typo

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name: {type : String, required: true},
     email: {type : String, required: true, unique: true},
     imageUrl: {type : String, required: true},
-    address: {type : String, required: true},
+    address: {type : String, required: false},
     cartItems: { type:Object, default: {} }
 }, { minimize: false })
 
