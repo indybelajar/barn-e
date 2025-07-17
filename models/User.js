ee917cd8278fb60c7f4b5701e9e62507dbc56b68
indybelajar <indyagustin06@gmail.com>
2025-07-17 12:14:36 +0700

    benerin typo

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
     _id: { type : String, required: true},
-    name: {type : Strinf, required: true},
+    name: {type : String, required: true},
     email: {type : String, required: true, unique: true},
     imageUrl: {type : String, required: true},
     address: {type : String, required: true},
